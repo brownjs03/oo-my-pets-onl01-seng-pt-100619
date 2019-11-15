d76881d1e0b827bdce348a52565fa095c8df9be0
Jeffrey Brown <brownjs03@gmail.com>
2019-11-15 23:33:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,8 +51,8 @@ class Owner
   end 
   
   def sell_pets 
-    Dog.all.each {|dog| dog.mood = "nervous"}
-    Cat.all.each {|cat| cat.mood = "nervous"}
+    Dog.all.each {|dog| dog.mood = "nervous" && dog.owner.clear}
+    Cat.all.each {|cat| cat.mood = "nervous" && cat.owner.clear}
   end 
   
 end
